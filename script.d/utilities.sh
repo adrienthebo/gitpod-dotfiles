89cf0c1dcfe498d936e12c7f7f2932ad3a319f06
Adrien Thebo <adrien@lagrange-automation.io>
2022-07-15 19:41:28 +0000

    Add tldr

--- script.d/utilities.sh
+++ script.d/utilities.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+install_http_binary() {
+    binary="$1"
+    url="$2"
+    
+    local output_path="$HOME/.local/bin/$binary"
+    
+    if ! [[ -f "$output_path" ]]; then
+        curl -L "$url" > "$output_path" && chmod +x "$output_path"
+    fi
+}
+
+install_http_binary tldr "https://github.com/dbrgn/tealdeer/releases/download/v1.5.0/tealdeer-linux-x86_64-musl"
+
 install_jiq() {
     curl -L https://github.com/fiatjaf/jiq/releases/download/v0.7.2/jiq_linux_amd64 -o "$HOME/.local/bin/jiq"
     chmod +x "$HOME/.local/bin/jiq"
