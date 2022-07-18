72e19ffdfe313d244cea5b6eabb7797a34d71c59
Adrien Thebo <adrien@lagrange-automation.io>
2022-07-18 15:39:05 +0000

    init_gcloud: automatically fetch gcloud binary via asdf

--- bashrc.d/gcp.sh
+++ bashrc.d/gcp.sh
@@ -3,17 +3,30 @@
 init_gcloud() {
     eval "$(gp env -e)"
 
+    if ! command -v gcloud; then
+        if ! [[ -d "$HOME/.asdf/plugins/gcloud" ]]; then
+            asdf plugin add gcloud
+        fi
+
+        local gcloud_version
+        gcloud_version="$(asdf list all gcloud | tail -n1)"
+        asdf install gcloud "$gcloud_version"
+        asdf global gcloud "$gcloud_version"
+    fi
+
     if [[ -f "$HOME/.config/gcloud/configurations/config_gitpod" ]]; then
         gcloud config configurations activate gitpod
     else
         gcloud config configurations create gitpod
     fi
     # Or activate
-    
+
+    gcloud auth login
+
     if [[ -n "$GCLOUD_ACCOUNT" ]]; then
         gcloud config set account "$(gp env | awk -F =  '/GCLOUD_ACCOUNT/ { print $2 }')"
     fi
-    
+
     if [[ -n "$GCLOUD_PROJECT" ]]; then
         gcloud config set core/project "$(gp env | awk -F =  '/GCLOUD_PROJECT/ { print $2 }')"
     fi
