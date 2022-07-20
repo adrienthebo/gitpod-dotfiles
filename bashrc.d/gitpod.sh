6e63a1d12b396d2ccc6cc730283aadd0a61c510c
Adrien Thebo <adrien@lagrange-automation.io>
2022-07-20 18:55:20 +0000

    Improve workspace-local environment variables

--- bashrc.d/gitpod.sh
+++ bashrc.d/gitpod.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-eval $(gp env -e)
+eval "$(gp env -e)"
+
+if [[ -f "$GITPOD_REPO_ROOT/.env" ]]; then
+    echo "Loading environment variables from $GITPOD_REPO_ROOT/.env"
+    set -x
+    source "$GITPOD_REPO_ROOT/.env"
+    set +x
+fi
+
+reload_dotfiles() {
+    source "$HOME/.dotfiles/script/reload"
+}
