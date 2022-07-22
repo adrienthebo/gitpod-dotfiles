ca4cf645a697eca356919d34fdbf0fceba19b78f
Adrien Thebo <adrien@lagrange-automation.io>
2022-07-22 18:02:55 +0000

    Feature init/load seems to work

--- lib/feature.sh
+++ lib/feature.sh
@@ -8,7 +8,7 @@ register_feature() {
     init_fn="$3"
     load_fn="$4"
     autoload_fn="$5"
-    
+
     _features[$name]="$name $status_fn $init_fn $load_fn $autoload_fn"
 }
 
@@ -19,13 +19,48 @@ announce_features() {
 
         declare -a _feature
         read -ra _feature <<< "${_features[$key]}"
-        
+
         f_name="${_feature[0]}"
         status_fn="${_feature[1]}"
-        
+
         status=$($status_fn)
         echo -e "\t$f_name: $status"
     done
+}
+
+load_feature() {
+    local key
+    key=$1
+
+    local _feature f_name status_fn init_fn load_fn autoload_fn
+
+    f_name="${_feature[0]}"
+    status_fn=${_feature[1]}
+    load_fn=${_feature[3]}
+
+    $load_fn
+    status=$($status_fn)
+    echo -e "\t$f_name: ${status}"
+}
+
+init_feature() {
+    local key
+    key=$1
+
+    local _feature f_name status_fn init_fn load_fn autoload_fn
+
+
+    declare -a _feature
+    read -ra _feature <<< "${_features[$key]}"
+
+    f_name="${_feature[0]}"
+    status_fn=${_feature[1]}
+    init_fn=${_feature[2]}
+
+    $init_fn
+    $load_fn
+    status=$($status_fn)
+    echo -e "\t$f_name: ${status}"
 }
 
 autoload_features() {
@@ -35,12 +70,12 @@ autoload_features() {
 
         declare -a _feature
         read -ra _feature <<< "${_features[$key]}"
-        
+
         f_name="${_feature[0]}"
         status_fn=${_feature[1]}
         load_fn=${_feature[3]}
         autoload_fn=${_feature[4]}
-        
+
         $autoload_fn
         if [[ $? -eq 0 ]]; then
             echo -e "\t$f_name: autoloading"
@@ -74,4 +109,40 @@ _f_autoload_gcloud() {
     fi
 }
 
+_f_asdf_status() {
+    if ! [[ -d $HOME/.asdf ]]; then
+        echo "absent"
+        return 1
+    elif [[ -z $ASDF_DIR ]]; then
+        echo "inactive"
+        return 1
+    else
+        echo "ready"
+        return 0
+    fi
+}
+
+_f_asdf_init() {
+    echo "initialing asdf"
+
+    if ! [[ -d ~/.asdf ]]; then
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+        source ~/.asdf/asdf.sh
+    fi
+}
+
+_f_asdf_load() {
+    source ~/.asdf/asdf.sh
+    return 0
+}
+
+_f_asdf_autoload() {
+    if [[ -n $FEATURE_ASDF_DISABLE ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 register_feature "gcloud" "_f_status_gcloud" "_f_init_gcloud" "_f_load_gcloud" "_f_autoload_gcloud"
+register_feature "asdf" "_f_asdf_status" "_f_asdf_init" "_f_asdf_load" "_f_asdf_autoload"
