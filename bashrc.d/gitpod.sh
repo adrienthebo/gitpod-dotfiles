da95f7ea923c37558ca7a6d5a8fd38ca6066aace
Adrien Thebo <adrien@lagrange-automation.io>
2022-08-03 22:55:24 +0000

    Alias commonly used kots commands

--- bashrc.d/gitpod.sh
+++ bashrc.d/gitpod.sh
@@ -12,3 +12,16 @@ fi
 reload_dotfiles() {
     source "$HOME/.dotfiles/script/reload"
 }
+
+alias kotsup="kubectl kots install gitpod --namespace gitpod --shared-password=gitpod --no-port-forward"
+
+kotsdash() {
+    local __kotsport="$(( ( $RANDOM % 63535 ) + 2000 ))"
+
+    (
+        sleep 2
+        $BROWSER "http://localhost:$__kotsport"
+    ) &
+
+    kubectl kots admin-console --namespace gitpod --port "$__kotsport"
+}
