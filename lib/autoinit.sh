b52d476497f19e3558a765f39b4c967c0c682473
Adrien Thebo <adrien@lagrange-automation.io>
2022-09-22 18:11:35 +0000

    [autoinit] remove duplicated logging functions

--- lib/autoinit.sh
+++ lib/autoinit.sh
@@ -8,40 +8,6 @@ source "${__LIBDIR}/colors.sh"
 source "${__LIBDIR}/autoinit-logging.sh"
 
 
-__autoinit_debug() {
-    if [[ -n $__AUTOINIT_DEBUG ]]; then
-        echo "$(color grey "$@")" 1>&2
-    fi
-}
-
-
-__autoinit_warn() {
-    # shellcheck disable=SC2005
-    echo "$(color yellow bold "Warning: $@")" 1>&2
-}
-
-
-__autoinit_error() {
-    # shellcheck disable=SC2005
-    echo "$(color red bold "Error: $@")" 1>&2
-}
-
-
-__autoinit_info() {
-    # shellcheck disable=SC2005
-    echo "$(color green "$@")" 1>&2
-}
-
-
-__autoinit_notice() {
-    echo "$(color blue "$@")" 1>&2
-}
-
-__autoinit_log() {
-    echo "$@" 1>&2
-}
-
-
 __autoinit_handle() {
     local argv="$@"
     local cmd="$1"
