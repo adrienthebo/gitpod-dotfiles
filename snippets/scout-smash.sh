7dbd704a1c1b7ea7e8acf1c4c8d6a49d22356941
Adrien Thebo <adrien@lagrange-automation.io>
2022-12-21 23:56:45 +0000

    smaaaash

--- snippets/scout-smash.sh
+++ snippets/scout-smash.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+kubectl get configmap gitpod-app -ojsonpath='{.data.app\.yaml}' | kubectl delete -f -
+
 aws ssm delete-parameter --name /cell/$CELL_NAME/service/rds || true
 
 leeway build bootstrap/lambdas:application
@@ -12,4 +14,8 @@ TF_ENFORCE=1 leeway run bootstrap/deployment/stages/05_lambdas:run
 export CELL_INSTALLER_IMAGE="eu.gcr.io/gitpod-core-dev/build/installer:aledbf-labels.17"
 
 leeway run bootstrap/lambdas/application/addons/manifests:publish-addons-bundle   -DclusterName="${CELL_NAME}-meta" -DclusterType=meta
+
+echo "Waiting 30s for addons to install"
+sleep 30
+
 leeway run bootstrap/lambdas/application:publish-installer-bundle   -DclusterName="${CELL_NAME}-meta" -DclusterType=meta   -Dkind=webapp   -Dimage=$CELL_INSTALLER_IMAGE
