483804c73cd500f362a89ef5c72181f5dc2483a6
Adrien Thebo <adrien@lagrange-automation.io>
2022-12-27 18:07:32 +0000

    [script.d] starship add

---
+++ script.d/60_starship.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+curl -sS https://starship.rs/install.sh | sh -s - --yes
+
+starship_init='eval "$(starship init bash)"'
+if ! grep -q "$starship_init" ~/.bashrc; then
+  echo "$starship_init" >> ~/.bashrc
+fi
+
+mkdir -p "$HOME/.config" || true
+cat - > ~/.config/starship.toml <<'EOD'
+format = """
+$kubernetes\
+$directory\
+$vcsh\
+$git_branch\
+$git_commit\
+$git_state\
+$git_metrics\
+$git_status\
+$docker_context\
+$golang\
+$helm\
+$nodejs\
+$php\
+$pulumi\
+$rust\
+$terraform\
+$aws\
+$gcloud\
+$azure\
+$sudo\
+$cmd_duration\
+$line_break\
+$jobs\
+$os\
+$character"""
+EOD
